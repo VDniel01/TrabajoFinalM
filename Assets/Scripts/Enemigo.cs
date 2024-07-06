29f0deb0c837778ed499f92c3aba2b232edfaa1d
VDniel01 <89489261+VDniel01@users.noreply.github.com>
2024-07-05 23:29:35 -0300

    quitar

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -7,6 +7,7 @@ public class Enemigo : MonoBehaviour
     public List<Transform> waypoints = new List<Transform>();
     private int targetIndex = 1;
     public float movementSpeed = 4;
+    public float rotationSpeed = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,28 @@ public class Enemigo : MonoBehaviour
     void Update()
     {
         Movement();
+        LookAt();
     }
 
     private void Movement()
     {
         transform.position = Vector3.MoveTowards(transform.position, waypoints[targetIndex].position, movementSpeed * Time.deltaTime);
+        var distance = Vector3.Distance(transform.position, waypoints[targetIndex].position);
+        if (distance <= 0.1f)
+        {
+            if (targetIndex >= waypoints.Count -1)
+            {
+                Debug.Log("subir targetindex");
+                return;
+            }
+            targetIndex++;
+        }
+    }
+    private void LookAt()
+    {
+        //transform.LookAt(waypoints[targetIndex]);
+        var dir = waypoints[targetIndex].position - transform.position;
+        var rootTarget = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rootTarget, rotationSpeed * Time.deltaTime);
     }
 }
